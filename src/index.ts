4014faace85f71be1d03464ba96dcc3d2d2bf01c
Dchole <derekoware47@gmail.com>
2024-07-21 23:52:12 +0000

    fix: use custom display for appearing element

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,41 @@
+type Display =
+  | "block"
+  | "inline"
+  | "inline-block"
+  | "flex"
+  | "grid"
+  | "flow-root"
+  | "none"
+  | "contents"
+  | "table"
+  | "table-row"
+  | "table-cell"
+  | "table-caption"
+  | "table-column"
+  | "table-column-group"
+  | "table-footer-group"
+  | "table-header-group"
+  | "table-row-group"
+  | "list-item"
+  | "inline-flex"
+  | "inline-grid"
+  | "inline-table"
+  | "run-in"
+  | "ruby"
+  | "ruby-base"
+  | "ruby-text"
+  | "ruby-base-container"
+  | "ruby-text-container"
+  | "subgrid" // CSS Grid Level 2
+  | "initial"
+  | "inherit"
+  | "unset";
+
 export interface Options {
   disableDelay?: boolean;
   keepLeavingElementMounted?: boolean;
+  leavingElementDisplayType?: Display;
+  enteringElementDisplayType?: Display;
 }
 
 interface Component {
@@ -32,9 +67,9 @@ export function transitionSwap(
     leavingOptions
   );
 
-  function revealEnteringElement() {
+  function revealEnteringElement(displayType: Display = "block") {
     // Show the entering element
-    enteringElement.style.display = "block";
+    enteringElement.style.display = displayType;
 
     // Apply the entering animation
     enteringElement.animate(enteringKeyframes, enteringOptions);
@@ -54,11 +89,11 @@ export function transitionSwap(
     }
 
     if (!options?.disableDelay) {
-      revealEnteringElement();
+      revealEnteringElement(options?.enteringElementDisplayType);
     }
   };
 
   if (options?.disableDelay) {
-    revealEnteringElement();
+    revealEnteringElement(options?.enteringElementDisplayType);
   }
 }
